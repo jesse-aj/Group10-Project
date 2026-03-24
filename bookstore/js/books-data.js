15f4bed770f71e881f84753526f1f63e954d401b
jesse-aj <appiahjesse123@gmail.com>
2026-03-24 08:00:00 +0000

    Initial commit - project structure and base files

---
+++ bookstore/js/books-data.js
@@ -0,0 +1,111 @@
+const books = [
+
+  // Self-Help Books
+  {
+    id: 1,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: 85,
+    category: "self-help",
+    image: "images/atomic-habits.jpg",
+    whatsapp: "233XXXXXXXXX"
+  },
+  {
+    id: 2,
+    title: "The 7 Habits of Highly Effective People",
+    author: "Stephen Covey",
+    price: 90,
+    category: "self-help",
+    image: "images/7-habits.jpg",
+    whatsapp: "233XXXXXXXXX"
+  },
+  {
+    id: 3,
+    title: "Think and Grow Rich",
+    author: "Napoleon Hill",
+    price: 75,
+    category: "self-help",
+    image: "images/think-grow-rich.jpg",
+    whatsapp: "233XXXXXXXXX"
+  },
+
+  // Christian Books
+  {
+    id: 4,
+    title: "The Purpose Driven Life",
+    author: "Rick Warren",
+    price: 80,
+    category: "christian",
+    image: "images/purpose-driven.jpg",
+    whatsapp: "233XXXXXXXXX"
+  },
+  {
+    id: 5,
+    title: "Mere Christianity",
+    author: "C.S. Lewis",
+    price: 70,
+    category: "christian",
+    image: "images/mere-christianity.jpg",
+    whatsapp: "233XXXXXXXXX"
+  },
+
+  // Fiction Books
+  {
+    id: 6,
+    title: "Things Fall Apart",
+    author: "Chinua Achebe",
+    price: 65,
+    category: "fiction",
+    image: "images/things-fall-apart.jpg",
+    whatsapp: "233YYYYYYYYYY"
+  },
+  {
+    id: 7,
+    title: "Half of a Yellow Sun",
+    author: "Chimamanda Ngozi Adichie",
+    price: 75,
+    category: "fiction",
+    image: "images/half-yellow-sun.jpg",
+    whatsapp: "233YYYYYYYYYY"
+  },
+
+  // Academic Books
+  {
+    id: 8,
+    title: "Discrete Mathematics and Its Applications",
+    author: "Kenneth Rosen",
+    price: 120,
+    category: "academic",
+    image: "images/discrete-maths.jpg",
+    whatsapp: "233YYYYYYYYYY"
+  },
+  {
+    id: 9,
+    title: "Introduction to Algorithms",
+    author: "Cormen et al.",
+    price: 150,
+    category: "academic",
+    image: "images/algorithms.jpg",
+    whatsapp: "233YYYYYYYYYY"
+  },
+  {
+    id: 10,
+    title: "Computer Networking: A Top-Down Approach",
+    author: "Kurose & Ross",
+    price: 130,
+    category: "academic",
+    image: "images/networking.jpg",
+    whatsapp: "233YYYYYYYYYY"
+  }
+
+
+  
+];
+
+
+
+
+
+
+
+//Dont forget to add some ghanians self help books (It is your Fault richie mensah and Richard Nii Armag Quayes book, kwame nkrumah and all)
